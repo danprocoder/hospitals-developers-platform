e0f0f63149c2d2d03f4b1683497af34faeb8c402
Daniel Austin <danielaustin718@gmail.com>
2019-09-22 22:23:22 +0100

    Setup react and typescript

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+class App extends React.Component {
+  render () {
+    return <div>Welcome to the developers platform</div>
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js"
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader"
+          }
+        ]
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader"
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html'
+    })
+  ]
+};
