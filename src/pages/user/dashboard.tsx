9f45f91f7b5a207e9decac347275713ca72f00fd
Daniel Austin <danielaustin718@gmail.com>
2019-09-23 22:39:10 +0100

    Copy API key to clipboard

--- src/pages/user/dashboard.tsx
+++ src/pages/user/dashboard.tsx
@@ -1,13 +1,29 @@
 import * as React from 'react'
 import { withRouter } from 'react-router'
+import * as Toastr from 'toastr'
 import * as Cookie from 'js-cookie'
 import { Copy as CopyIcon } from 'react-feather'
 import api, { ApiErrorResponse } from '../../utils/http.ts'
 import Template from './templates/default.tsx'
 
+Toastr.options.timeOut = 1000
+
 const userToken = Cookie.get('user-token')
 
-function UserDashboard (props: any): JSX.Element {
+function copyToClipboard (apiKey: string) {
+  const input = document.createElement('input')
+  input.value = apiKey
+  document.body.appendChild(input)
+
+  input.select()
+  document.execCommand('copy')
+
+  document.body.removeChild(input)
+
+  Toastr.success('API key copied to clipboard')
+}
+
+function UserDashboard (): JSX.Element {
   const [apiKeys, setApiKeys] = React.useState([])
 
   function loadApiKeys (): void {
@@ -16,7 +32,6 @@ function UserDashboard (props: any): JSX.Element {
         headers: { authorization: `Bearer ${userToken}` }
       })
       .then((response: any) => {
-        console.log(response.data.data)
         setApiKeys(response.data.data)
       })
       .catch((error: ApiErrorResponse) => console.error(error.response))
@@ -27,24 +42,24 @@ function UserDashboard (props: any): JSX.Element {
   }, [])
 
   return (
-    <Template>
-      <div>
-        <h4>My Access Tokens</h4>
+      <Template>
         <div>
-          {apiKeys.map((key, index) => (
-            <div key={index}>
-              <div>{key.project}</div>
-              <div>
-                {key.apiKey}
-                <a href='#'>
-                  <CopyIcon />
-                </a>
+          <h4>My Access Tokens</h4>
+          <div>
+            {apiKeys.map((key, index) => (
+              <div key={index}>
+                <div>{key.project}</div>
+                <div>
+                  <span>{key.apiKey}</span>
+                  <button onClick={() => copyToClipboard(key.apiKey)}>
+                    <CopyIcon />
+                  </button>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
-    </Template>
+      </Template>
   )
 }
 
